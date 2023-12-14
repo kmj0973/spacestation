fc9574adc8b8132e07a6214b8ccb677dcbfd29b3
[SW7] 홍소현 <hshyeon86@gmail.com>
2023-12-14 05:08:15 +0000

    Update server/src/follow/follow.controller.ts, server/src/follow/follow.router.ts, server/src/follow/follow.service.ts files

--- server/src/follow/follow.controller.ts
+++ server/src/follow/follow.controller.ts
@@ -1,4 +1,3 @@
-// TODO: 프로젝트 구조를 보여주기위한 샘플 파일임. 구조 잡히면 지우기.
 import { Request } from "express";
 import asyncHandler from "../middleware/asyncHandler.js";
 import { CustomError } from "../middleware/errorHandler.js";
