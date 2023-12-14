bfc462ccb3b588b7382b960b32f4dace3e379a45
HSHyeon <hshyeon86@gmail.com>
2023-12-14 14:17:28 +0900

    feat: 팔로우 스키마(follow) 작성

--- server/src/follow/follow.service.ts
+++ server/src/follow/follow.service.ts
@@ -1,4 +1,3 @@
-// TODO: 프로젝트 구조를 보여주기위한 샘플 파일임. 구조 잡히면 지우기.
 import FollowModel from "./follow.model.js";
 
 const followService = {
@@ -6,12 +5,7 @@ const followService = {
     return FollowModel.find({});
   },
 
-  async postFollow({follower}: { follower:string}) {
-    return FollowModel.create({ follower });
-  },
-  async deleteFollow({follower}:{follower:string}) {
-    return FollowModel.deleteOne({follower});
-  },
+ 
 };
 
 export default followService;
